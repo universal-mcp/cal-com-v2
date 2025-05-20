b499ecd9f3e4078a5e8eaea225ac0d97422af0d1
Ankit Ranjan <arstejas7@gmail.com>
2025-05-20 17:57:04 +0530

    minor fix

--- src/universal_mcp_cal_com_v2/mcp.py
+++ src/universal_mcp_cal_com_v2/mcp.py
@@ -6,7 +6,7 @@ from universal_mcp.stores import EnvironmentStore
 from universal_mcp_cal_com_v2.app import CalComV2App
 
 env_store = EnvironmentStore()
-integration_instance = ApiKeyIntegration(name="CAL-COM-V2_API_KEY", store=env_store)
+integration_instance = ApiKeyIntegration(name="CAL_COM_V2_API_KEY", store=env_store)
 app_instance = CalComV2App(integration=integration_instance)
 
 mcp = SingleMCPServer(
